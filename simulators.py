3706e04347dba0a6d80f7ffe0d8e346a86051ab5
Kirill Sechkar <kirill.sechkar@queens.ox.ac.uk>
2026-09-07 14:03:55 +0100

    Improved jit, give up if small

--- simulators.py
+++ simulators.py
@@ -8,6 +8,7 @@ jax.config.update("jax_enable_x64", True)
 import numpy as np
 import scipy.integrate
 import jax.numpy as jnp
+import functools
 
 
 # SOLVERS --------------------------------------------------------------------------------------------------------------
@@ -18,7 +19,8 @@ def loopy_euler(model_ode,
                 sim_state_record,
                 ode_step,
                 ode_steps_in_savetimestep,
-                args):
+                args,
+                if_small_give_up, small_threshold):
     """
         Use Euler integration to get the simulator state to be recorded at the current saving point into
         sim_state_record - to be used with jax.lax.fori_loop
@@ -30,25 +32,43 @@ def loopy_euler(model_ode,
             ode_step: simulation time step duration
             ode_steps_in_savetimestep: number of ODE integration steps between each recording
             args: additional arguments to pass to the ODE function
+            if_small_give_up: indices of state variables to check for giving up if they are too small
+            small_threshold: threshold for giving up if any of the state variables is too small
 
         Returns:
             new_sim_state_record: record of simulation time points and state vector values - now with the right x at xs[save_cntr,:]
     """
 
-    # EULER STEP FUNCTION (with non-negativity constraints)
-    def euler_step(step_cntr, t_x):
-        return {
-            # entries updated over the course of the Euler step
-            't': t_x['t'] + ode_step,
-            'x': jnp.maximum(t_x['x'] + ode_step * model_ode(t_x['t'], t_x['x'], args),0),
-        }
-    # GET THE PRESENT TIME POINT AND STATE
-    last_t_x = {'t': sim_state_record['ts'][save_cntr-1], 'x': sim_state_record['xs'][save_cntr-1, :]}
-    this_t_x = jax.lax.fori_loop(0, ode_steps_in_savetimestep, euler_step, last_t_x)
+    # FUNCTION FOR GETTING THE NEW SIMULATOR STATE IF WE DON'T GIVE UP
+    def euler_carryon(sim_state_record_carryon):
+        # EULER STEP FUNCTION (with non-negativity constraints)
+        def euler_step(step_cntr, t_x):
+            return {
+                # entries updated over the course of the Euler step
+                't': t_x['t'] + ode_step,
+                'x': jnp.maximum(t_x['x'] + ode_step * model_ode(t_x['t'], t_x['x'], args), 0),
+            }
 
-    # RETURN UPDATED SIMULATOR STATE
-    new_sim_state_record = {'ts': sim_state_record['ts'], 'xs': sim_state_record['xs'].at[save_cntr, :].set(this_t_x['x'])}
-    return new_sim_state_record
+        # GET THE PRESENT TIME POINT AND STATE
+        last_t_x = {'t': sim_state_record_carryon['ts'][save_cntr - 1],
+                    'x': sim_state_record_carryon['xs'][save_cntr - 1, :]}
+        this_t_x = jax.lax.fori_loop(0, ode_steps_in_savetimestep, euler_step, last_t_x)
+
+        # FIND IF WE SHOULD GIVE UP NEXT
+        give_up = jnp.any(jnp.where(if_small_give_up, this_t_x['x'] <= small_threshold, False))
+
+        # RETURN UPDATED SIMULATOR STATE
+        new_sim_state_record = {'ts': sim_state_record_carryon['ts'],
+                                'xs': sim_state_record_carryon['xs'].at[save_cntr, :].set(this_t_x['x']),
+                                'give_up': give_up}
+        return new_sim_state_record
+
+    # FUNCTION FOR NOT DOING ANYTHING IF WE GIVE UP
+    def euler_give_up(sim_state_record_giveup):
+        return sim_state_record_giveup
+
+    # RETURN EITHER THE UPDATED SIMULATOR STATE OR JUST THE OLD STATE IF WE GAVE UP
+    return jax.lax.cond(sim_state_record['give_up'], euler_give_up, euler_carryon, sim_state_record)
 
 # Fourth-order Runge-Kutta solver using jax.lax.fori_loop
 def loopy_rk4(model_ode,
@@ -56,7 +76,9 @@ def loopy_rk4(model_ode,
               sim_state_record,
               ode_step,
               ode_steps_in_savetimestep,
-              args):
+              args,
+              if_small_give_up, small_threshold
+              ):
     """
         Use fourth-order Runge-Kutta integration to get the simulator state to be recorded at the current saving point
         into sim_state_record - to be used with jax.lax.fori_loop
@@ -68,33 +90,51 @@ def loopy_rk4(model_ode,
             ode_step: simulation time step duration
             ode_steps_in_savetimestep: number of ODE integration steps between each recording
             args: additional arguments to pass to the ODE function
+            if_small_give_up: indices of state variables to check for giving up if they are too small
+            small_threshold: threshold for giving up if any of the state variables is too small
 
         Returns:
             new_sim_state_record: record of simulation time points and state vector values - now with the right x at xs[save_cntr,:]
     """
 
-    # FOURTH-ORDER RUNGE-KUTTA STEP FUNCTION (with non-negativity constraints)
-    def rk4_step(step_cntr, t_x):
-        k1 = model_ode(t_x['t'], t_x['x'], args)
-        k2 = model_ode(t_x['t'] + ode_step / 2, jnp.maximum(t_x['x'] + ode_step * k1 / 2, 0), args)
-        k3 = model_ode(t_x['t'] + ode_step / 2, jnp.maximum(t_x['x'] + ode_step * k2 / 2, 0), args)
-        k4 = model_ode(t_x['t'] + ode_step, jnp.maximum(t_x['x'] + ode_step * k3, 0), args)
-        return {
-            # entries updated over the course of the Runge-Kutta step
-            't': t_x['t'] + ode_step,
-            'x': jnp.maximum(t_x['x'] + ode_step * (k1 + 2 * k2 + 2 * k3 + k4) / 6, 0),
-        }
+    # FUNCTION FOR GETTING THE NEW SIMULATOR STATE IF WE DON'T GIVE UP
+    def rk4_carryon(sim_state_record_carryon):
+        # FOURTH-ORDER RUNGE-KUTTA STEP FUNCTION (with non-negativity constraints)
+        def rk4_step(step_cntr, t_x):
+            k1 = model_ode(t_x['t'], t_x['x'], args)
+            k2 = model_ode(t_x['t'] + ode_step / 2, jnp.maximum(t_x['x'] + ode_step * k1 / 2, 0), args)
+            k3 = model_ode(t_x['t'] + ode_step / 2, jnp.maximum(t_x['x'] + ode_step * k2 / 2, 0), args)
+            k4 = model_ode(t_x['t'] + ode_step, jnp.maximum(t_x['x'] + ode_step * k3, 0), args)
+            return {
+                # entries updated over the course of the Runge-Kutta step
+                't': t_x['t'] + ode_step,
+                'x': jnp.maximum(t_x['x'] + ode_step * (k1 + 2 * k2 + 2 * k3 + k4) / 6, 0),
+            }
 
-    # GET THE PRESENT TIME POINT AND STATE
-    last_t_x = {'t': sim_state_record['ts'][save_cntr-1], 'x': sim_state_record['xs'][save_cntr-1, :]}
-    this_t_x = jax.lax.fori_loop(0, ode_steps_in_savetimestep, rk4_step, last_t_x)
+        # GET THE PRESENT TIME POINT AND STATE
+        last_t_x = {'t': sim_state_record_carryon['ts'][save_cntr - 1], 'x': sim_state_record_carryon['xs'][save_cntr - 1, :]}
+        this_t_x = jax.lax.fori_loop(0, ode_steps_in_savetimestep, rk4_step, last_t_x)
 
-    # RETURN UPDATED SIMULATOR STATE
-    new_sim_state_record = {'ts': sim_state_record['ts'], 'xs': sim_state_record['xs'].at[save_cntr, :].set(this_t_x['x'])}
-    return new_sim_state_record
+        # FIND IF WE SHOULD GIVE UP NEXT
+        give_up = jnp.any(jnp.where(if_small_give_up, this_t_x['x'] <= small_threshold, False))
+
+        # RETURN UPDATED SIMULATOR STATE
+        new_sim_state_record = {'ts': sim_state_record_carryon['ts'],
+                                'xs': sim_state_record_carryon['xs'].at[save_cntr, :].set(this_t_x['x']),
+                                'give_up': give_up}
+        return new_sim_state_record
+
+    # FUNCTION FOR NOT DOING ANYTHING IF WE GIVE UP
+    def rk4_give_up(sim_state_record_giveup):
+        return sim_state_record_giveup
+
+    # RETURN EITHER THE UPDATED SIMULATOR STATE OR JUST THE OLD STATE IF WE GAVE UP
+    return jax.lax.cond(sim_state_record['give_up'], rk4_give_up, rk4_carryon, sim_state_record)
 
 
 # SIMULATOR FUNCTION ---------------------------------------------------------------------------------------------------
+# call simulator function around jit
+
 # simulator function
 def sim(model_ode, args, x0, tf, savetimestep, simulator='rk4', return_numpy=True, **kwargs):
     """
@@ -114,79 +154,20 @@ def sim(model_ode, args, x0, tf, savetimestep, simulator='rk4', return_numpy=Tru
             xs: system state at each time point specfied
     """
 
-    if (simulator == 'euler'):
-        # define custom Euler parameters if not specified
-        ## number of ODE integration steps between each record point
+    if (simulator == 'euler' or simulator == 'rk4'):
+        # see whether we should give up if any of the state variables is small, and the threshold for doing so
+        if_small_give_up = kwargs.get('if_small_give_up', jnp.zeros(len(x0), dtype=jnp.bool_))
+        small_threshold = kwargs.get('small_threshold', 0.0)
+
+        # determine number of ode steps in savetimestep
         ode_steps_in_savetimestep = kwargs.get('ode_steps_in_savetimestep', 1e4)
 
-        # define the time points at which we save the solution
-        ts = jnp.concatenate((jnp.arange(tf[0], tf[1], savetimestep), jnp.array([tf[1]])), dtype=jnp.float64)
-
-        # calculate the time step for the Euler integration
-        ode_step = savetimestep / ode_steps_in_savetimestep
-
-        # make the model ode function return a jnp.array
-        model_ode_jnp = lambda t, x, args: jnp.array(model_ode(t, x, args))
-
-        # make the retrieval of next x a lambda-function for jax.lax.scanning
-        loop_step = lambda save_cntr, sim_state_record: loopy_euler(model_ode_jnp,
-                                                                    save_cntr,
-                                                                    sim_state_record,  # simulator state
-                                                                    ode_step,  # simulation time step
-                                                                    int(ode_steps_in_savetimestep), # number of ODE integration steps between each recording
-                                                                    args)
-
-        # initalise the simulator state: (t, x) - x initialised with initial conditions
-        sim_state_record = {'ts': ts, 'xs': jnp.tile(jnp.array(x0), (ts.shape[0], 1))}
-
-        # simulate
-        sim_state_rec_final = jax.lax.fori_loop(1, ts.shape[0], loop_step, sim_state_record)
-        xs = sim_state_rec_final['xs']
-
-        # check for simulation success (i.e. no nans or infs in x)
-        success = not bool(jnp.any(jnp.isnan(xs)) or jnp.any(jnp.isinf(xs)))
+        # call the jitted simulation function
+        ts, xs, success = sim_to_jit(model_ode, args, x0, tf, savetimestep, ode_steps_in_savetimestep, simulator, if_small_give_up, small_threshold)
 
         # return numpy or jax.numpy arrays
         if(return_numpy):
-            return np.array(ts), np.array(xs), success
-        else:
-            return ts, xs, success
-
-    elif (simulator == 'rk4'):
-        # define custom fourth-order Runge-Kutta parameters if not specified
-        ## number of ODE integration steps between each record point
-        ode_steps_in_savetimestep = kwargs.get('ode_steps_in_savetimestep', 1e4)
-
-        # define the time points at which we save the solution
-        ts = jnp.concatenate((jnp.arange(tf[0], tf[1], savetimestep), jnp.array([tf[1]])), dtype=jnp.float64)
-
-        # calculate the time step for the fourth-order Runge-Kutta integration
-        ode_step = savetimestep / ode_steps_in_savetimestep
-
-        # make the model ode function return a jnp.array
-        model_ode_jnp = lambda t, x, args: jnp.array(model_ode(t, x, args))
-
-        # make the retrieval of next x a lambda-function for jax.lax.fori_loop
-        loop_step = lambda save_cntr, sim_state_record: loopy_rk4(model_ode_jnp,
-                                                                  save_cntr,
-                                                                  sim_state_record,  # simulator state
-                                                                  ode_step,  # simulation time step
-                                                                  int(ode_steps_in_savetimestep), # number of ODE integration steps between each recording
-                                                                  args)
-
-        # initalise the simulator state: (t, x) - x initialised with initial conditions
-        sim_state_record = {'ts': ts, 'xs': jnp.tile(jnp.array(x0), (ts.shape[0], 1))}
-
-        # simulate
-        sim_state_rec_final = jax.lax.fori_loop(1, ts.shape[0], loop_step, sim_state_record)
-        xs = sim_state_rec_final['xs']
-
-        # check for simulation success (i.e. no nans or infs in x)
-        success = not bool(jnp.any(jnp.isnan(xs)) or jnp.any(jnp.isinf(xs)))
-
-        # return numpy or jax.numpy arrays
-        if (return_numpy):
-            return np.array(ts), np.array(xs), success
+            return np.array(ts), np.array(xs), bool(success)
         else:
             return ts, xs, success
 
@@ -201,7 +182,7 @@ def sim(model_ode, args, x0, tf, savetimestep, simulator='rk4', return_numpy=Tru
         ## ODE solver
         solver = kwargs.get('solver', 'LSODA')
         ## ODE integration tolerances to specify the step size controller
-        tols = kwargs.get('tols', {'rtol': 1e-6, 'atol': 1e-9})
+        tols = kwargs.get('tols', {'rtol': 1e-6, 'atol': 1e-6})
         ## initial time step
         dt0 = kwargs.get('dt0', 0.1)
 
@@ -223,3 +204,104 @@ def sim(model_ode, args, x0, tf, savetimestep, simulator='rk4', return_numpy=Tru
 
     else:
         raise ValueError("Unknown simulator: {}".format(simulator))
+
+# sub-function to be jax.jitted for jax-enabled simulations
+@functools.partial(
+      jax.jit,
+      static_argnames=(
+          "model_ode",
+          "tf",
+          "savetimestep",
+          "simulator",
+          "ode_steps_in_savetimestep"
+      ))
+def sim_to_jit(model_ode, args, x0, tf, savetimestep, ode_steps_in_savetimestep, simulator, 
+               if_small_give_up, small_threshold):
+    """
+        Simulate an ODE model.
+
+        Args:
+            model_ode: ODE function handle
+            args: extra arguments for the ODE function
+            x0: initial condition
+            tf: time span of the simulation
+            savetimestep: saving the siulation every savetimestep hours
+            ode_steps_in_savetimestep
+            simulator: simulation method
+            if_small_give_up: indices of state variables to check for giving up if they are too small
+            small_threshold: threshold for giving up if any of the state variables is too small
+
+        Returns:
+            xs: system state at each time point specfied
+    """
+    if (simulator == 'euler'):
+        # define the time points at which we save the solution
+        ts = jnp.concatenate((jnp.arange(tf[0], tf[1], savetimestep), jnp.array([tf[1]])), dtype=jnp.float64)
+
+        # calculate the time step for the Euler integration
+        ode_step = savetimestep / ode_steps_in_savetimestep
+
+        # make the model ode function return a jnp.array
+        model_ode_jnp = lambda t, x, args: jnp.array(model_ode(t, x, args))
+
+        # make the retrieval of next x a lambda-function for jax.lax.scanning
+        loop_step = lambda save_cntr, sim_state_record: loopy_euler(model_ode_jnp,
+                                                                    save_cntr,
+                                                                    sim_state_record,  # simulator state
+                                                                    ode_step,  # simulation time step
+                                                                    int(ode_steps_in_savetimestep), # number of ODE integration steps between each recording
+                                                                    args,
+                                                                    if_small_give_up, small_threshold)
+
+        # initalise the simulator state: (t, x) - x initialised with initial conditions
+        sim_state_record = {'ts': ts, 'xs': jnp.tile(jnp.array(x0), (ts.shape[0], 1)), 'give_up': False}
+
+        # simulate
+        sim_state_rec_final = jax.lax.fori_loop(1, ts.shape[0], loop_step, sim_state_record)
+        xs = sim_state_rec_final['xs']
+
+        # check for simulation success (i.e. no nans or infs in x)
+        # check for simulation success (i.e. no nans or infs in x, no giving up)
+        success = jnp.logical_not(
+            jnp.logical_or(
+                jnp.logical_or(jnp.any(jnp.isnan(xs)), jnp.any(jnp.isinf(xs))),
+                sim_state_rec_final['give_up']
+            )
+        )
+
+    elif (simulator == 'rk4'):
+        # define the time points at which we save the solution
+        ts = jnp.concatenate((jnp.arange(tf[0], tf[1], savetimestep), jnp.array([tf[1]])), dtype=jnp.float64)
+
+        # calculate the time step for the fourth-order Runge-Kutta integration
+        ode_step = savetimestep / ode_steps_in_savetimestep
+
+        # make the model ode function return a jnp.array
+        model_ode_jnp = lambda t, x, args: jnp.array(model_ode(t, x, args))
+
+        # make the retrieval of next x a lambda-function for jax.lax.fori_loop
+        loop_step = lambda save_cntr, sim_state_record: loopy_rk4(model_ode_jnp,
+                                                                  save_cntr,
+                                                                  sim_state_record,  # simulator state
+                                                                  ode_step,  # simulation time step
+                                                                  int(ode_steps_in_savetimestep), # number of ODE integration steps between each recording
+                                                                  args,
+                                                                  if_small_give_up, small_threshold)
+
+        # initalise the simulator state: (t, x) - x initialised with initial conditions
+        sim_state_record = {'ts': ts, 'xs': jnp.tile(jnp.array(x0), (ts.shape[0], 1)), 'give_up': False}
+
+        # simulate
+        sim_state_rec_final = jax.lax.fori_loop(1, ts.shape[0], loop_step, sim_state_record)
+        xs = sim_state_rec_final['xs']
+
+        # check for simulation success (i.e. no nans or infs in x, no giving up)
+        success = jnp.logical_not(
+            jnp.logical_or(
+                jnp.logical_or(jnp.any(jnp.isnan(xs)),jnp.any(jnp.isinf(xs))),
+                sim_state_rec_final['give_up']
+            )
+        )
+
+    # return ts, xs, and success as jax.numpy arrays
+    return ts, xs, success
